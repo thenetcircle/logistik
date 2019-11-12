1232e061a8cf314ebe8a3c46e050273f9f11e52f
Oscar Eriksson <oscar.eriks@gmail.com>
2019-11-12 17:22:27 +0800

    failed_topic not failed

--- logistik/handlers/manager.py
+++ logistik/handlers/manager.py
@@ -77,7 +77,7 @@ class HandlersManager(IHandlersManager):
             raise QueryException(response.status_code)
 
         json_response = response.json()
-        fields = ['return_to', 'event', 'method', 'retries', 'timeout', 'group_id', 'path', 'failed']
+        fields = ['return_to', 'event', 'method', 'retries', 'timeout', 'group_id', 'path', 'failed_topic']
 
         try:
             for field in fields:
