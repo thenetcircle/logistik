743192107080937ea1bfa363296d838ad856860e
Oscar Eriksson <oscar@thenetcircle.com>
2020-06-16 16:30:15 +0800

    about to tag version 1.0.0

--- second.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(
-    import_name=__name__
-)
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-version = '1.0.0-dev'
+version = '1.0.0'
 
 setup(
     name='logistik',
