40c79fe46d6549ddb5f658d29158bf22c549e9b5
Oscar Eriksson <oscar@thenetcircle.com>
2023-05-19 17:00:29 +0800

    edit handler using the cli

--- bin/cli.py
+++ bin/cli.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
-import random
+import datetime
 import sys
-from typing import Optional
-
-from prompt_toolkit.validation import Validator, ValidationError
-
 sys.path.insert(0, '')
 
 import ast
+import csv
 import os
+import random
 import re
 from string import Template
-from sqlalchemy import create_engine, Engine
-from tabulate import tabulate
-from pprint import pprint
+from typing import Optional
+
+import sqlalchemy
 import yaml
+from PyInquirer import prompt, Separator
+from prompt_toolkit.validation import Validator, ValidationError
+from sqlalchemy import create_engine, Engine
 from sqlalchemy import text
-from PyInquirer import prompt
+from tabulate import tabulate
 
 from logistik.config import ConfigKeys
 from logistik.environ import ConfigDict
@@ -130,6 +131,9 @@ class PathValidator(Validator):
             )
 
 
+NAME_IDX = 1
+ID_IDX = 0
+
 LIST_QUERY = """
 select 
     id, name, event, return_to, endpoint, 
@@ -163,6 +167,16 @@ values (
 returning id;
 """
 
+EDIT_QUERY = """
+update 
+    handler_conf_entity
+set
+    service_id = :service_id, name = :name, event = :event, endpoint = :endpoint, hostname = :hostname, port = :port,
+    path = :path, node = :node, return_to = :return_to, group_id = :group_id
+where
+    id = :id;
+"""
+
 handler_keys = [
     "ID",
     "Name",
@@ -194,18 +208,35 @@ initial_options = [
         'name': 'action',
         'message': 'Action',
         'choices': [
-            'List', 'Edit', 'Add', 'Disable', 'Enable'
-        ],
-        'filter': lambda val: val.lower()
+            'list',
+            'add',
+            'edit',
+            Separator(),
+            {
+                'name': 'disable',
+                'disabled': 'Unavailable at this time'
+            },
+            {
+                'name': 'enable',
+                'disabled': 'Unavailable at this time'
+            }
+        ]
     },
     {
         'type': 'input',
         'name': 'filter',
-        'message': 'Filter by handler name (blank to skip)?',
+        'message': 'Filter by handler name (blank to skip):',
         'default': '',
-        'when': lambda answers: answers['action'] == 'list'
+        'when': lambda answers: answers['action'] in {'edit', 'list', 'disable'}
     }
 ]
+
+choose_options = [{
+    'type': 'list',
+    'name': 'handler',
+    'message': 'Handler:',
+    'choices': []
+}]
 
 add_options = [
     {
@@ -270,19 +301,86 @@ add_options = [
 
 
 def main():
+    print()
     options = prompt(initial_options)
     env = load_env(options['environment'])
     env.engine = create_db(env)
     print()
 
     if options['action'] == 'list':
-        list_handlers(env, options['filter'])
+        handlers = list_handlers(env, options['filter'])
+        print()
+        print(tabulate(
+            handlers,
+            headers=[
+                'ID', 'Name', 'Topic', 'Return-To', 'IP', 'Port', 'Path', 'Node', 'Group ID'
+            ],
+            tablefmt='fancy_grid'
+        ))
 
     elif options['action'] == 'add':
         add_handler(env)
 
+    elif options['action'] == 'edit':
+        handlers = list_handlers(env, options['filter'])
+        edit_handler(env, handlers)
+
     else:
         print('not implemented yet')
+
+
+def edit_handler(env: Environment, handlers: list):
+    backup_db(env)
+
+    options = choose_options.copy()
+    handler_name_to_handler = dict()
+
+    for handler in handlers:
+        key = f"ID: {handler[ID_IDX]} NAME: {handler[NAME_IDX]}"
+        options[0]["choices"].append(key)
+        handler_name_to_handler[key] = handler
+
+    values = prompt(options)
+
+    # cancelled by user
+    if not len(values):
+        return
+
+    handler_to_edit = handler_name_to_handler[values["handler"]]
+    edit_options = add_options.copy()
+
+    try:
+        assert int(handler_to_edit[0]) > 0
+    except Exception as e:
+        print(f"invalid handler ID '{handler_to_edit[0]}': {str(e)}")
+        return
+
+    for i in range(len(edit_options[0])):
+        # +1 because index 0 is the id
+        edit_options[i]['default'] = str(handler_to_edit[i+1])
+
+    values = prompt(edit_options)
+
+    with env.engine.begin() as connection:
+        connection.execute(
+            text(EDIT_QUERY),
+            {
+                "service_id": values.get('name'),
+                "name": values.get('name'),
+                "event": values["event"],
+                "endpoint": values.get('endpoint'),
+                "hostname": values.get('endpoint'),
+                "port": values.get('port'),
+                "path": values.get('path'),
+                "node": values.get('node'),
+                "return_to": values.get('return_to'),
+                "group_id": values.get('group_id'),
+                "id": handler_to_edit[0]
+            }
+        )
+
+    # index 0 is the id
+    print_handler(env, handler_to_edit[0])
 
 
 def add_handler(env: Environment):
@@ -320,25 +418,7 @@ def add_handler(env: Environment):
 
         [row_id] = result.fetchone()
 
-    with env.engine.connect() as connection:
-        values = connection.execute(
-            text(SELECT_QUERY),
-            {
-                "id": row_id
-            }
-        )
-
-    new_handler = list()
-
-    for row in values:
-        for key, value in zip(handler_keys, row):
-            new_handler.append((key, value))
-        break
-
-    print()
-    print("New handler added:")
-    print()
-    print(tabulate(new_handler, tablefmt='fancy_grid'))
+    print_handler(env, row_id)
 
 
 def list_handlers(env: Environment, filter_name: str):
@@ -352,21 +432,13 @@ def list_handlers(env: Environment, filter_name: str):
         error("Could not connect to db", e)
         return
 
-    name_idx = 1
     handlers = list()
     for handler in all_handlers:
-        if len(filter_name) and filter_name not in handler[name_idx]:
+        if len(filter_name) and filter_name not in handler[NAME_IDX]:
             continue
         handlers.append(handler)
 
-    print()
-    print(tabulate(
-        handlers,
-        headers=[
-            'ID', 'Name', 'Topic', 'Return-To', 'IP', 'Port', 'Path', 'Node', 'Group ID'
-        ],
-        tablefmt='fancy_grid'
-    ))
+    return handlers
 
 
 def create_db(env: Environment) -> Engine:
@@ -412,6 +484,45 @@ def load_env(env_name: str) -> Environment:
     env_conf = ast.literal_eval(template)
 
     return Environment(ConfigDict(env_conf))
+
+
+def print_handler(env: Environment, row_id: int):
+    with env.engine.connect() as connection:
+        values = connection.execute(
+            text(SELECT_QUERY),
+            {
+                "id": row_id
+            }
+        )
+
+    new_handler = list()
+
+    for row in values:
+        for key, value in zip(handler_keys, row):
+            new_handler.append((key, value))
+        break
+
+    print()
+    print("New handler added:")
+    print()
+    print(tabulate(new_handler, tablefmt='fancy_grid'))
+
+
+def backup_db(env: Environment):
+    """
+    before any editing is done, backup the conf table locally
+    """
+    with env.engine.connect() as connection:
+        result = connection.execute(text("select * from handler_conf_entity;"))
+
+    timestamp = datetime.datetime.utcnow().strftime("%Y%m%d-%H%M%S")
+    filename = f'logistik-conf-backup-{timestamp}.csv'
+
+    with open(filename, 'w') as f:
+        output_file = csv.writer(f)
+        output_file.writerow(result.keys())
+        for row in result:
+            output_file.writerow(row)
 
 
 def error(s, e):
