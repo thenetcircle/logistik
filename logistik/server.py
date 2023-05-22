6b0f3fa5ffe90f4319e24f1b0cc177b58116a958
Oscar Eriksson <oscar@thenetcircle.com>
2023-05-22 17:43:46 +0800

    updaed requirements

--- logistik/server.py
+++ logistik/server.py
@@ -5,7 +5,6 @@ from uuid import uuid4 as uuid
 from flask import Flask
 from flask_restful import Api
 from flask_cors import CORS
-from werkzeug.contrib.fixers import ProxyFix
 
 from logistik import environ
 from logistik.config import ConfigKeys
@@ -30,42 +29,6 @@ logger.setLevel(log_level)
 logging.getLogger("kafka").setLevel(logging.WARNING)
 logging.getLogger("kafka.conn").setLevel(logging.WARNING)
 logging.getLogger("urllib3.connectionpool").setLevel(logging.WARNING)
-
-
-class ReverseProxied(object):
-    """
-    Wrap the application in this middleware and configure the
-    front-end server to add these headers, to let you quietly bind
-    this to a URL other than / and to an HTTP scheme that is
-    different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
-    """
-
-    def __init__(self, _app):
-        self.app = _app
-
-    def __call__(self, env, start_response):
-        script_name = env.get("HTTP_X_SCRIPT_NAME", "")
-        if script_name:
-            env["SCRIPT_NAME"] = script_name
-            path_info = env["PATH_INFO"]
-            if path_info.startswith(script_name):
-                env["PATH_INFO"] = path_info[len(script_name) :]
-
-        scheme = env.get("HTTP_X_SCHEME", "")
-        if scheme:
-            env["wsgi.url_scheme"] = scheme
-        return self.app(env, start_response)
 
 
 def create_app():
@@ -98,8 +61,6 @@ def create_app():
         static_folder="admin/static/",
     )
     env.cors = CORS(_app, resources={r"/api/*": {"origins": "*"}})
-
-    _app.wsgi_app = ReverseProxied(ProxyFix(_app.wsgi_app))
 
     _app.config["ROOT_URL"] = env.config.get(
         ConfigKeys.ROOT_URL, domain=ConfigKeys.WEB, default="/"
--- logistik/web.py
+++ logistik/web.py
@@ -3,7 +3,6 @@ from uuid import uuid4 as uuid
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from werkzeug.contrib.fixers import ProxyFix
 
 from logistik import environ
 from logistik.config import ConfigKeys
@@ -14,42 +13,6 @@ logger = logging.getLogger(__name__)
 logging.getLogger("kafka.conn").setLevel(logging.INFO)
 logging.getLogger("kafka.client").setLevel(logging.INFO)
 logging.getLogger("kafka.metrics").setLevel(logging.INFO)
-
-
-class ReverseProxied(object):
-    """
-    Wrap the application in this middleware and configure the
-    front-end server to add these headers, to let you quietly bind
-    this to a URL other than / and to an HTTP scheme that is
-    different than what is used locally.
-
-    In nginx:
-    location /myprefix {
-        proxy_pass http://192.168.0.1:5001;
-        proxy_set_header Host $host;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header X-Scheme $scheme;
-        proxy_set_header X-Script-Name /myprefix;
-        }
-
-    :param app: the WSGI application
-    """
-
-    def __init__(self, _app):
-        self.app = _app
-
-    def __call__(self, env, start_response):
-        script_name = env.get("HTTP_X_SCRIPT_NAME", "")
-        if script_name:
-            env["SCRIPT_NAME"] = script_name
-            path_info = env["PATH_INFO"]
-            if path_info.startswith(script_name):
-                env["PATH_INFO"] = path_info[len(script_name) :]
-
-        scheme = env.get("HTTP_X_SCHEME", "")
-        if scheme:
-            env["wsgi.url_scheme"] = scheme
-        return self.app(env, start_response)
 
 
 def create_app():
@@ -80,8 +43,6 @@ def create_app():
     _app.config["SQLALCHEMY_DATABASE_URI"] = "{}://{}:{}@{}:{}/{}".format(
         db_drvr, db_user, db_pass, db_host, db_port, db_name
     )
-
-    _app.wsgi_app = ReverseProxied(ProxyFix(_app.wsgi_app))
 
     return _app, SQLAlchemy(_app)
 
